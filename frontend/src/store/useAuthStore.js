5b777e47734707c0ea6d08a92dc6dd3ef5c96248
SurajPatil2645 <surajpatil2645@gmail.com>
2026-09-10 11:40:21 +0530

    login & logout implemented

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,6 +6,7 @@ export const useAuthStore = create((set) => ({
     authUser: null,
     isCheckingAuth: true,
     isSigningUp: false,
+    isLoggedIn: false,
 
     checkAuth: async () => {
         try {
@@ -19,17 +20,41 @@ export const useAuthStore = create((set) => ({
         }
     },
 
-    signup: async(data) => {
-        set({isSigningUp: true})
+    signup: async (data) => {
+        set({ isSigningUp: true })
         try {
             const res = await axiosInstance.post("/auth/signup", data);
-            set({ authUser: res.data});
+            set({ authUser: res.data });
 
             toast.success("Account created successfully!");
         } catch (error) {
             toast.error(error.response.data.message);
-        } finally{
-            set({isSigningUp: false})
+        } finally {
+            set({ isSigningUp: false })
+        }
+    },
+
+    login: async (data) => {
+        set({ isLoggedIn: true })
+        try {
+            const res = await axiosInstance.post('/auth/login', data);
+            set({ authUser: res.data })
+
+            toast.success("Login successfully");
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isLoggedIn: false });
+        }
+    },
+
+    logout: async() => {
+        try {
+            await axiosInstance.post('/auth/logout');
+            set({authUser: null});
+            toast.success("Logged out successfully");
+        } catch (error) {
+            toast.error("Error logging out", error);
         }
     }
 }));
